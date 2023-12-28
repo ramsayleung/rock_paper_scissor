b8bde31b903269d8cf1d613bac3815a0536fd5d6
Ramsay Leung <ramsayleung@gmail.com>
2023-12-27 20:51:27 -0800

    Initial Commit

---
+++ RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by ramsayleung on 2023-12-27.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
